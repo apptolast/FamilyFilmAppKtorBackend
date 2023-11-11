b9ffe74d09ea918ff8c544772272f656b5fd6f77
Coshiloco <pablohurtadohg@gmail.com>
2023-11-11 14:07:13 +0100

    JWT token

--- src/main/kotlin/com/example/auth/AuthRoutes.kt
+++ src/main/kotlin/com/example/auth/AuthRoutes.kt
@@ -1,29 +1,37 @@
 package com.example.auth
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import com.example.auth.models.JwtConfig
-import com.example.auth.models.User
+import com.example.auth.models.LoginRequest
+import com.example.auth.models.RegisterRequest
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.util.Date
+import io.ktor.http.*
 
+fun Application.authRoutes(jwtConfig: JwtConfig) {
+    val authService = AuthService(jwtConfig)
 
-fun Route.authRoutes(jwtConfig: JwtConfig) {
-    post("/login") {
-        val user = call.receive<User>()
-        val token = JWT.create()
-            .withAudience(jwtConfig.audience)
-            .withIssuer(jwtConfig.issuer)
-            .withClaim("username", user.username)
-            .withExpiresAt(Date(System.currentTimeMillis() + 60000))
-            .sign(Algorithm.HMAC256(jwtConfig.secret))
-        call.respond(hashMapOf("token" to token))
-    }
+    routing {
+        post("/login") {
+            val loginRequest = call.receive<LoginRequest>()
+            val user = authService.authenticate(loginRequest)
+            if (user != null) {
+                val token = authService.generateToken(user)
+                call.respond(hashMapOf("token" to token))
+            } else {
+                call.respond(HttpStatusCode.Unauthorized, "Invalid credentials")
+            }
+        }
 
-    post("/register") {
-
+        post("/register") {
+            val registerRequest = call.receive<RegisterRequest>()
+            val registered = authService.register(registerRequest)
+            if (registered) {
+                call.respond(HttpStatusCode.Created, "User created")
+            } else {
+                call.respond(HttpStatusCode.BadRequest, "User already exists")
+            }
+        }
     }
 }
--- src/main/kotlin/com/example/auth/AuthService.kt
+++ src/main/kotlin/com/example/auth/AuthService.kt
@@ -1,15 +1,54 @@
 package com.example.auth
 
+import com.example.auth.models.JwtConfig
+
 import com.example.auth.models.User
+import com.example.auth.models.LoginRequest
+import com.example.auth.models.RegisterRequest
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import java.util.Date
 
+class AuthService(private val jwtConfig: JwtConfig) {
 
-class AuthService {
+    // Simula una base de datos en memoria
+    private val users = mutableListOf<User>()
 
-    fun authenticate(ussername: String, password: String): User? {
+    fun authenticate(loginRequest: LoginRequest): User? {
+        val user = users.find { it.username == loginRequest.username }
+        // Aquí deberías comparar la contraseña hasheada
+        if (user != null && user.passwordHash == hashPassword(loginRequest.password)) {
+            return user
+        }
         return null
     }
 
-    fun register(user: User): Boolean {
+    fun register(registerRequest: RegisterRequest): Boolean {
+        if (users.any { it.username == registerRequest.username }) {
+            return false // El usuario ya existe
+        }
+        // Aquí deberías hashear la contraseña antes de almacenarla
+        val newUser = User(
+            id = users.size + 1, // Esto es solo un ejemplo, deberías tener un ID único adecuado
+            username = registerRequest.username,
+            email = registerRequest.email,
+            passwordHash = hashPassword(registerRequest.password)
+        )
+        users.add(newUser)
         return true
+    }
+
+    fun generateToken(user: User): String {
+        return JWT.create()
+            .withAudience(jwtConfig.audience)
+            .withIssuer(jwtConfig.issuer)
+            .withClaim("username", user.username)
+            .withExpiresAt(Date(System.currentTimeMillis() + 3600000)) // 1 hora de validez
+            .sign(Algorithm.HMAC256(jwtConfig.secret))
+    }
+
+    private fun hashPassword(password: String): String {
+        // Aquí deberías implementar un algoritmo de hasheo seguro
+        return password.reversed() // Esto es solo un ejemplo y no debe usarse en producción
     }
 }
